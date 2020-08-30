f1504d95348bbbc8289447b2d16eb6c58502eb77
Sam Maus <samueldmaus@gmail.com>
2020-08-30 15:33:28 -0500

    updated styling on add movie page

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
+import './AddMovie.css';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import Button from '@material-ui/core/Button'
+import { FormControl } from '@material-ui/core';
+import InputLabel from '@material-ui/core/InputLabel'
 
 class AddMovie extends Component {
     state = {
@@ -64,17 +70,25 @@ class AddMovie extends Component {
     render() {
         return (
             <form onSubmit={this.addMovie}>
-                <input name='title' placeholder="Title" value={this.state.title} onChange={this.handleChange}/>
-                <input name='poster'placeholder="Image Path/URL" value={this.state.poster} onChange={this.handleChange}/>
-                <input name='description' placeholder="Description" value={this.state.description} onChange={this.handleChange} />
-                
-                <select id='add_genre' onChange={this.selectedId}>
-                    {this.props.reduxState.genres.map(genre=> (
-                        <option key={genre.id} value={genre.id}>{genre.name}</option>
-                    ))}
-                </select>
-                <button type='submit'>Add Movie</button>
-                <button onClick={this.cancelAdd}>Cancel</button>
+                <TextField name='title' label="Title" value={this.state.title} onChange={this.handleChange}/>
+                <br/>
+                <TextField name='poster' label="Image Path/URL" value={this.state.poster} onChange={this.handleChange}/>
+                <br/>
+                <TextField multiline rows={6} name='description' label="Description" value={this.state.description} onChange={this.handleChange} />
+                <br/>
+                <FormControl >
+                    <InputLabel>Genre</InputLabel>
+                    <NativeSelect label='Genre' id='add_genre' onChange={this.selectedId}>
+                        <option aria-label="None" value="" />
+                        {this.props.reduxState.genres.map(genre=> (
+                            <option key={genre.id} value={genre.id}>{genre.name}</option>
+                        ))}
+                    </NativeSelect>
+                </FormControl>
+                <br/>
+                <br/>
+                <Button variant="contained" color="secondary" type='submit'>Add Movie</Button>
+                <Button variant="contained" color="secondary" onClick={this.cancelAdd}>Cancel</Button>
             </form>
         )
     }
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import {IconButton} from '@material-ui/core';
