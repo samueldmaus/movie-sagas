09a5686916c3b2bb06be04094e578b1cb50c427b
Sam Maus <samueldmaus@gmail.com>
2020-08-30 12:53:02 -0500

    adding movie functinoality working

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -1,35 +1,68 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import axios from 'axios';
 
 class AddMovie extends Component {
     state = {
-        newMovie: {
-            title: '',
-            image: '',
-            description: '',
-            genre: ''
-        }
+
+        title: '',
+        poster: '',
+        description: '',
+        genre_id: '',
+        
     };
 
     componentDidMount() {
         this.props.dispatch({type: "FETCH_GENRES"});
     }
 
+    handleChange = (event) => {
+        this.setState({
+            ...this.state,
+            [event.target.name]: event.target.value
+        })
+    };
+
+    selectedId = () => {
+        this.setState({
+            ...this.state,
+            genre_id: document.getElementById('add_genre').value
+        })
+    }
+
+    addMovie = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+        axios.post('/api/movie', this.state)
+        .then(response => {
+            console.log('ADDED TO DB');
+            this.setState({
+                title: '',
+                poster: '',
+                description: '',
+                genre_id: '',
+            })
+        }).catch(error => {
+            console.log('error in POST:', error)
+        })
+    }
+        
+
+
     render() {
         return (
-            <div>
-                <input placeholder="Title" value={this.state.newMovie.title} />
-                <input placeholder="Image Path/URL" value={this.state.newMovie.image} />
-                <input placeholder="Description" value={this.state.newMovie.description} />
-                <form>
-                    <select name='add_genre'>
-                        {this.props.reduxState.genres.map(genre=> (
-                            <option key={genre.id} value={genre.name}>{genre.name}</option>
-                        ))}
-                    </select>
-                </form>
-                <button>Add Movie</button>
-            </div>
+            <form onSubmit={this.addMovie}>
+                <input name='title' placeholder="Title" value={this.state.title} onChange={this.handleChange}/>
+                <input name='poster'placeholder="Image Path/URL" value={this.state.poster} onChange={this.handleChange}/>
+                <input name='description' placeholder="Description" value={this.state.description} onChange={this.handleChange} />
+                
+                <select id='add_genre' onChange={this.selectedId}>
+                    {this.props.reduxState.genres.map(genre=> (
+                        <option key={genre.id} value={genre.id}>{genre.name}</option>
+                    ))}
+                </select>
+                <button type='submit'>Add Movie</button>
+            </form>
         )
     }
 }
